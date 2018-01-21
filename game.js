26273458f8bcece5853559b4606020d49280b1b1
Jordan Asporaat <31646973+j-asporaat@users.noreply.github.com>
2018-01-21 22:52:33 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
+var random = Math.floor( Math.random() * 479);
+var answer = words[random];
+var answerArray = answer.split('');
 
+console.log(answer);
+
+var checkBtn = document.getElementById('check');
+
+checkBtn.setAttribute("onclick", "check();");
+
+function check(){
+
+  for(var i = 0; i <=4; i++){
+
+  var input = document.getElementById("letter_" + i );
+  console.log(input.value);
+
+  if(input.value == answerArray[i]) {
+    input.style.backgroundColor = "red";
+      }
+    }
+  }
